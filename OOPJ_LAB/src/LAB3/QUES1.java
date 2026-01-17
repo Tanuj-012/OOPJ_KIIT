e5c1ac9b251b27beff25adaff6364f69df35d729
Tanuj-012 <tanujgvtd@gmail.com>
2026-01-17 17:27:42 +0530

    2D_3D PLASTIC_RATE

---
+++ OOPJ_LAB/src/LAB3/QUES1.java
@@ -0,0 +1,71 @@
+package LAB3;
+
+import java.security.PublicKey;
+import java.util.Scanner;
+
+class plastic2d {
+    double l, b;
+
+    public void read() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---ENTER DIMENSION---");
+        System.out.println("ENTER LENGTH:");
+        l = sc.nextDouble();
+        System.out.println("ENTER BREADTH:");
+        b = sc.nextDouble();
+    }
+
+    public double calculate() {
+        return l * b * 40;
+    }
+}
+
+class plastic3d extends plastic2d {
+    double h;
+    public void read() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER HEIGHT:");
+        h = sc.nextDouble();
+    }
+    public double calculate()
+    {
+        return l*b*h*60;
+    }
+}
+
+public class QUES1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        System.out.println("ENTER YOUR CHOICE:\n1:2D SHEET\n2: 3D SHEET");
+        choice= sc.nextInt();
+        switch (choice)
+        {
+            case 1:
+            {
+                plastic2d sheet= new plastic2d();
+                sheet.read();
+                double value=sheet.calculate();
+                System.out.println("COST FOR 2D SHEET: Rs"+value);
+                break;
+            }
+            case 2:
+            {
+                plastic3d sheet= new plastic3d();
+                sheet.read();
+                double value=sheet.calculate();
+                System.out.println("COST FOR 3D SHEET: Rs"+value);
+                break;
+            }
+            default:
+            {
+                    System.out.println("ENTER VALID CHOICE");
+
+            }
+
+        }
+
+
+    }
+}
